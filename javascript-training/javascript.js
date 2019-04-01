c1d203e183528e418b912b126ae547da9fb30152
river <riverhuang011@gmail.com>
2019-04-01 11:52:43 +0700

    object

--- javascript-training/javascript.js
+++ javascript-training/javascript.js
@@ -48,26 +48,140 @@ document.write("<br/> str"+ str)
 
 
 
-if (!Array.prototype.filter){
-    Array.prototype.filter= function(fun/*, this*/){
-        var len= this.length;
+    if (!Array.prototype.filter){
+        Array.prototype.filter= function(fun/*, this*/){
+            var len= this.length;
+            if (typeof fun != "function")
+            throw new TypeError();
+            var res = newArray();
+            var thisp = arguments[1];
+            for ( i=0;i< len; i++){
+                if (i in this){
+                    var val= this[i]; //in case fun mutates this
+                if (fun.call(thisp, val, i, this))
+                res.push(val)
+                
+                }
+            }
+            return res;
+        };
+    }
+    function isBigEnough(element,index,array){
+        return(element>=10)
+    }
+    var filtered =  [12,5,8,130,44].filter(isBigEnough);
+    document.write("Filtered Value:" +filtered);
+
+
+if(!Array.prototype.map){
+    Array.prototype.map = function (fun/*, thisp */){
+        var len = this.length;
         if (typeof fun != "function")
         throw new TypeError();
-        var res = newArray();
-        var thisp = arguments[1];
-        for ( i=0;i< len; i++){
-            if (i in this){
-                var val= this[i]; //in case fun mutates this
-            if (fun.call(thisp, val, i, this))
-            res.push(val)
-            
-            }
+        var res = new Array(len);
+        var thisp= arguments[1];
+        for(var i = 0; i <len; i++){
+            if(i in this)
+            res[i] = fun.call(thisp, this[i], i, this);
         }
         return res;
     };
 }
-function isBigEnough(element,index,array){
-    return(element>=10)
+var numbers = [1,4,9];
+var roots= numbers.map(Math.sqrt);
+document.write("root is:"+ roots);
+
+
+var numbers= new Array(1,4,9);
+var length = numbers.push(10);
+document.write("<br/> new number is:" + numbers);
+
+var numbers=[1,4,9];
+var element = numbers.pop();
+document.write("element is"+ element);
+var element= numbers.pop()
+document.write("<br/> element is:"+ element);
+
+var arr = new Array("mangga","apel","banana", "gori");
+var sorted = arr.sort();
+document.write("<br/> the sort is"+ sorted);
+
+var arr = [1,2,3].reverse();
+document.write("<br/> reversed array is"+arr);
+var arr = ["banana","mango","sugar","tea","milk"]
+document.write("<br/> the first slice is"+ arr.slice(1,2));
+document.write("<br/> the second slice is"+ arr.slice(1,5));
+
+function Warn(){
+    alert("this is a warning message!")
+    document.write("this is a warning message!")
 }
-var filtered =  [12,5,8,130,44].filter(isBigEnough);
-document.write("Filtered Value:" +filtered);
+
+function getConfirmation(){
+    var retVal= confirm("do you want to continue?");
+    if (retVal== true){
+        document.write("user wants to continue!");
+        return true;
+    } else{
+        document.write("users does not want to continue!");
+        return false;
+    }
+}
+
+function getValue(){
+    var retVal = prompt("enter your name:","your name here");
+    document.write("you have entered:"+retVal);
+}
+function myFunction (p1,p2){
+    return p1*p2;
+}
+document.getElementById("demo").innerHTML = myFunction(4,3);
+
+
+var x = myFunction(4,5);
+    document.getElementById("demo2").innerHTML = x;
+    function myFunction(a,b){
+        return a*b;
+    }
+function toCelcius(f){
+    return (5/9) * (f-32);
+}
+document.getElementById("demo3").innerHTML=toCelcius(77);
+
+var x = toCelcius(77);
+var text = "The temparature is "+ x+ "Celcius";
+var text = "the temperature is"+ toCelcius(77)+"Celcius";
+
+function myFunction(){
+     var carName ="Volvo";
+     document.getElementById("demo1").innerHTML =
+     typeof carName + " " +carName;
+}
+var car= "fiat";
+document.getElementById("demo").innerHTML = car;
+
+var car={type:"Fiat",model:"500", color:"white"}
+document.getElementById("demo").innerHTML = "the car type is" +  car.type;
+
+var person={
+    firstname:"river",
+    lastname:"huang",
+    age: 18,
+    eyecolor:"black"
+};
+document.getElementById("demo").innerHTML = person.firstname+ "years old.";
+document.getElementById("demo").innerHTML = person.firstname+ " " + person.lastname;
+document.getElementById("demo").innerHTML = person.firstname+ " " + person["lastname"];
+
+var person ={
+    firstname:"river",
+    lastname:"huang",
+    age: 18,
+    eyecolor: black,
+    id: 1831097,
+    fullname: function(){
+        return this.firstname+ " "+ this.lastname
+    }
+};
+document.getElementById("demo").innerHTML = person.fullname();
+document.getElementById("demo").innerHTML = person.fullname;
