417c0fd3f85582f2281c596ff85bc6f934322ba1
river <riverhuang011@gmail.com>
2019-04-15 00:19:24 +0700

    javascript

--- pratice3(javascript)/javascript.js
+++ pratice3(javascript)/javascript.js
@@ -18,24 +18,37 @@ const students = [{
 
 var person= students.filter(students =>students.grade >50)
 
-console.log(person)
+console.log(person)//filter
 
 var person1= students.map(students=> students +"gender:male")
-console.log(person1)
+console.log(person1)//map
 
-var person3 = "";
-students.forEach(element => {
-    person3 = person3 + element + students[0].grade ;
+// var person3 = "";
+// students.forEach(element => {
+//     person3 = person3 + element + students.grade ;
     
-});
-console.log(person3)
+// });
+// console.log(person3)
+
+
 function myFunction(){
-    students.sort(function(a,b){return a.grade-b.grade})
+    students.sort(function(a,b){return b.grade-a.grade})
+} //sort highest
+console.log(students.sort(myFunction));
+
+function myFunction1(){
+    students.sort(function(a,b){return a.name-b.name})
 }
 
-console.log(students.sort(myFunction));
-function myFunction(){
-    students.sort()
-}
+console.log(students.sort(myFunction1));//sort name
 
-console.log(students.sort(myFunction));
+function myFunction2(){
+    students.push({name:"julie", grade:70, gender:"female"})
+}
+console.log(students.push(myFunction2))
+
+
+//     function myFunction3(){
+//         students.pop(students=> students.grade<60)
+//     }
+// console.log(students.pop(myFunction3))
